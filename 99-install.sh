8a65f16921ede72bf4552503088c728d9eb7d45c
Ivan Trubach <mr.trubach@icloud.com>
2018-12-06 19:45:00 +0300

    Added install script

---
+++ 99-install.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+cp 00-index.md index.md
+cp 01-metadata.yaml metadata.yaml
+cp 02-template.html template.html
+cp 03-main.css main.css
+cp 04-md.js md.js
+cp 96-build.js build.js
+cp 97-package.json package.json
+cp 98-package-lock.json package-lock.json
