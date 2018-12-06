b4f6cf10b8efd166b3490f5ff1b729821ab3e327
Ivan Trubach <mr.trubach@icloud.com>
2018-12-06 20:15:06 +0300

    Added publish script

---
+++ 99-publish.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+cp index.md 00-index.md
+cp metadata.yaml 01-metadata.yaml
+cp template.html 02-template.html
+cp main.css 03-main.css
+cp md.js 04-md.js
+cp build.js 96-build.js
+cp package.json 97-package.json
+cp package-lock.json 98-package-lock.json
