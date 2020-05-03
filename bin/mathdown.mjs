5214c6d761d59fe85aa4eaf13ecd19e5447b7fc2
Ivan Trubach <mr.trubach@icloud.com>
2020-05-03 23:15:45 +0300

    Use process.argv instead of arg package

--- bin/mathdown.mjs
+++ bin/mathdown.mjs
@@ -3,7 +3,7 @@
 import '../lib/sandbox-patch.mjs'
 
 import fs from 'fs'
-import arg from 'arg'
+import util from 'util'
 import yaml from 'js-yaml'
 import handlebars from 'handlebars'
 
@@ -20,37 +20,99 @@ import tableofcontents from 'markdown-it-toc-done-right'
 
 import { version } from '../lib/version.mjs'
 
-const readFile = (path) => fs.readFileSync(path, 'utf8')
-const openFile = (path) => fs.openSync(path, 'w')
-const args = arg({
-	'-input': readFile,
-	'-output': openFile,
-	'-metadata': readFile,
-	'-template': readFile,
-})
-const [
-	input = readFile(0), // stdin
-	output = 1, // stdout
-	metadata = '',
-	template = '<doctype html>{{{body}}}',
-] = [
-	args['-input'],
-	args['-output'],
-	args['-metadata'],
-	args['-template'],
-]
+const flags = {
+	input: '',
+	output: '',
+	metadata: '',
+	template: '',
+	args: [],
+}
+const argv = process.argv
+const argc = argv.length
+if (argc < 2) {
+	console.error('not enough arguments')
+	process.exit(1)
+}
+for (let i = 2; i < argc; i++) {
+	if (!argv[i].startsWith('-') || argv[i] == '--') {
+		i++
+		flags.args = argv.slice(i)
+		break
+	}
+	switch (argv[i]) {
+	case '-input':
+		i++
+		if (i >= argc) {
+			console.error('expected input file path')
+			process.exit(1)
+		}
+		flags.input = argv[i]
+		break
+	case '-output':
+		i++
+		if (i >= argc) {
+			console.error('expected output file path')
+			process.exit(1)
+		}
+		flags.output = argv[i]
+		break
+	case '-metadata':
+		i++
+		if (i >= argc) {
+			console.error('expected metadata file path')
+			process.exit(1)
+		}
+		flags.metadata = argv[i]
+		break
+	case '-template':
+		i++
+		if (i >= argc) {
+			console.error('expected template file path')
+			process.exit(1)
+		}
+		flags.template = argv[i]
+		break
+	default:
+		const quoted = util.format('%O', argv[i])
+		console.error(`unknown argument: ${quoted}`)
+		process.exit(1)
+	}
+}
 
-anchor.defaults.permalink = true
+let inputFile = process.stdin.fd
+if (flags.input != '') {
+	inputFile = flags.input
+}
+const input = fs.readFileSync(inputFile, 'utf8')
+
+let outputFile = process.stdout.fd
+if (flags.output != '') {
+	outputFile = flags.output
+}
+const output = fs.openSync(outputFile, 'w')
+
+let metadataText = ''
+if (flags.metadata != '') {
+	metadataText = fs.readFileSync(flags.metadata, 'utf8')
+}
+const metadata = yaml.load(metadataText)
+
+let templateText = '<doctype html>{{{body}}}'
+if (flags.template != '') {
+	templateText = fs.readFileSync(flags.template, 'utf8')
+}
+const template = handlebars.compile(templateText)
 
 const config = {
 	typography: true,
 	highlight: (s) => `<pre class=microlight><code>${hl(s)}</code></pre>`,
 }
 
-const subst = handlebars.compile(template)
-fs.writeSync(output, subst({
+anchor.defaults.permalink = true
+
+fs.writeSync(output, template({
 	version: `v${version}`,
-	...yaml.load(metadata),
+	...metadata,
 	body: md(config)
 		.use(emoji)
 		.use(graph)
