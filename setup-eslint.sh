b06df4e09b588457067798751e7821623445eee5
Ivan Trubach <mr.trubach@icloud.com>
2020-05-09 14:13:34 +0300

    Use npm -C flag in setup-eslint.sh

--- setup-eslint.sh
+++ setup-eslint.sh
@@ -8,15 +8,14 @@
 # Below is a workaround to install this package from local folder.
 
 eslint_plugin_import=third_party/eslint-plugin-import
-(
+if ! [ -d $eslint_plugin_import ]; then
 	git clone -q --depth 1 https://github.com/benmosher/eslint-plugin-import $eslint_plugin_import
 	git -C $eslint_plugin_import checkout -q 92caa3594e0f8d7bf143dedba0c7c2b47b541f34
 
-	cd $eslint_plugin_import
-	npm -s install --no-save --ignore-scripts --no-progress --no-fund \
+	npm -C $eslint_plugin_import -s install --no-save --ignore-scripts --no-progress --no-fund \
 		rimraf@2.7.1 babel-cli@6.26.0 babel-preset-es2015-argon@0.1.0
-	npm -s run build
-)
+	npm -C $eslint_plugin_import -s run build
+fi
 
 exec npm -s install --no-save --ignore-scripts --no-progress --no-fund \
 	github:eslint/eslint#bd58eabd557ee38d03cd4fb65c0db1644d0dc843 \
